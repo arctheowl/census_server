9989f77063fcf7e3e51e9b34cdeb55bc84a22732
lidevlin <lidevlin95@gmail.com>
2023-01-13 22:58:23 +0000

    Using production vars

--- graph/utils.go
+++ graph/utils.go
@@ -1,35 +1,32 @@
 package graph
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
-func getEnvVar(key string) string {
-	err := godotenv.Load(".env")
+// func getEnvVar(key string) string {
+// 	err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	return os.Getenv(key)
-}
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	return os.Getenv(key)
+// }
 
-// Development env vars
-var (
-	host     = getEnvVar("PGHOST")
-	port     = getEnvVar("PGPORT")
-	user     = getEnvVar("PGUSER")
-	password = getEnvVar("PGPASSWORD")
-	dbname   = getEnvVar("PGDATABASE")
-)
+// // Development env vars
+// var (
+// 	host     = getEnvVar("PGHOST")
+// 	port     = getEnvVar("PGPORT")
+// 	user     = getEnvVar("PGUSER")
+// 	password = getEnvVar("PGPASSWORD")
+// 	dbname   = getEnvVar("PGDATABASE")
+// )
 
 // Production env vars
-// var (
-// 	host     = os.Getenv("PGHOST")
-// 	port     = os.Getenv("PGPORT")
-// 	user     = os.Getenv("PGUSER")
-// 	password = os.Getenv("PGPASSWORD")
-// 	dbname   = os.Getenv("PGDATABASE")
-// )
+var (
+	host     = os.Getenv("PGHOST")
+	port     = os.Getenv("PGPORT")
+	user     = os.Getenv("PGUSER")
+	password = os.Getenv("PGPASSWORD")
+	dbname   = os.Getenv("PGDATABASE")
+)
